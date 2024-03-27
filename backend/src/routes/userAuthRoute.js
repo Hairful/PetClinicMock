aca0fbb3abd4ea61075e6408e18f93dada123378
SundustYc <SundustYc@outlook.com>
2024-03-27 15:56:29 +0800

    [build]: remove cache

--- backend/src/routes/userAuthRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userAuthController = require('../controllers/userAuthController');
-
-router.post('/register', userAuthController.registerUser);
-router.post('/login', userAuthController.loginUser);
-
-module.exports = router;
