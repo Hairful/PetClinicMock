aa26fb2fd58939c1c4bfbf63f361a130b0b04dc1
shuaige1027 <138178611+shuaige1027@users.noreply.github.com>
2024-04-06 21:37:17 +0800

    [feat] add virtualtour functionshow and some system tests by jest

--- frontend/petclinicmock_frontend/tests/unit/example.spec.js
+++ frontend/petclinicmock_frontend/tests/unit/example.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from './test.vue'
+import HelloWorld from '../../src/views/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
