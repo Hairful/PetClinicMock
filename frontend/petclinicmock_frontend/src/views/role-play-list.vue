4afbf4e2f4e4a16db72bde8b736c0f0d667ac75a
Hairful <96092093+Hairful@users.noreply.github.com>
2024-04-07 12:14:23 +0800

    [style] 汉化角色扮演

--- frontend/petclinicmock_frontend/src/views/role-play-list.vue
+++ frontend/petclinicmock_frontend/src/views/role-play-list.vue
@@ -12,74 +12,15 @@
         <div data-thq="thq-navbar-nav" class="role-play-list-desktop-menu">
           <span>
             <span>
-              Logged in as
+              登录用户：
               <span v-html="rawiv06"></span>
+              <span class="role-play-list-text02">{{name}}</span>
             </span>
-            <span class="role-play-list-text02">{{name}}</span>
           </span>
         </div>
         <div>
-          <button style="margin-top: 10px;" class="buttonFilled" @click="logout"> logout </button>
+          <button style="margin-top: 10px;" class="buttonFilled" @click="logout"> 登出系统 </button>
         </div>
-      </div>
-        <div data-thq="thq-burger-menu" class="role-play-list-burger-menu">
-          <svg viewBox="0 0 1024 1024" class="role-play-list-icon socialIcons">
-            <path
-              d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"
-            ></path>
-          </svg>
-        </div>
-        <div
-          data-thq="thq-mobile-menu"
-          class="role-play-list-mobile-menu1 mobileMenu"
-        >
-          <div class="role-play-list-nav">
-            <div class="role-play-list-top">
-              <span class="logo">PETSCLINIC</span>
-              <div data-thq="thq-close-menu" class="role-play-list-close-menu">
-                <svg
-                  viewBox="0 0 1024 1024"
-                  class="role-play-list-icon02 socialIcons"
-                >
-                  <path
-                    d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"
-                  ></path>
-                </svg>
-              </div>
-            </div>
-            <nav class="role-play-list-links">
-              <span class="role-play-list-nav12 bodySmall">Home</span>
-              <span class="role-play-list-nav22 bodySmall">Services</span>
-              <span class="role-play-list-nav32 bodySmall">Our Team</span>
-              <span class="role-play-list-nav42 bodySmall">Contact</span>
-            </nav>
-            <div class="role-play-list-buttons">
-              <button class="buttonFlat">Login</button>
-              <button class="buttonFilled">Register</button>
-            </div>
-          </div>
-          <div>
-            <svg
-              viewBox="0 0 950.8571428571428 1024"
-              class="role-play-list-icon04 socialIcons"
-            >
-              <path
-                d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"
-              ></path></svg><svg
-              viewBox="0 0 877.7142857142857 1024"
-              class="role-play-list-icon06 socialIcons"
-            >
-              <path
-                d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"
-              ></path></svg><svg
-              viewBox="0 0 602.2582857142856 1024"
-              class="role-play-list-icon08 socialIcons"
-            >
-              <path
-                d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"
-              ></path>
-            </svg>
-          </div>
         </div>
       </header>
     </div>
@@ -88,7 +29,7 @@
       <div class="role-play-list-container2">
         <h1 class="role-play-list-hero-heading">
           <span class="heading1">
-            Role Play:
+            角色扮演：
           </span>
           <span class="role-play-list-text04">{{ this.role }}</span>
         </h1>
@@ -96,15 +37,15 @@
     </div>
     <div class="role-play-list-container3">
       <router-link to="/role-play-menu" class="role-play-list-navlink button">
-        Choose Role
+        重选角色
       </router-link>
-      <router-link to="/menu" class="role-play-list-navlink1 button">
-        Menu
+      <router-link to="/menu" class="role-play-list-navlink button">
+        返回菜单
       </router-link>
     </div>
     <div class="role-play-list-hero1 heroContainer">
       <h1 class="role-play-list-hero-heading1">
-        <span class="heading1">Choose a Job</span>
+        <span class="heading1">选择职责</span>
         <br />
       </h1>
       <div class="role-play-list-container4">
@@ -115,7 +56,7 @@
             class="role-play-list=li list-item Content">
             <router-link
               :to="{path:'/role-play-detail', query:{ role: role, job: job } }"
-              class="role-play-list-navlink button bodyLarge"
+              class="role-play-list-navlink2 button bodyLarge"
             >
               {{ job }}
             </router-link>
@@ -168,11 +109,11 @@ export default {
     },
     role2number(role) {
       switch (role) {
-        case 'Front Desk':
+        case '前台':
           return 0;
-        case 'Medical Assistant':
+        case '医助':
           return 1;
-        case 'Doctor':
+        case '医生':
           return 2;
         default:
           return -1;
@@ -374,17 +315,6 @@ export default {
   text-decoration: none;
   background-color: var(--dl-color-custom-primary2);
 }
-.role-play-list-navlink1 {
-  color: var(--dl-color-gray-white);
-  font-size: 20px;
-  align-self: center;
-  font-style: normal;
-  font-weight: 600;
-  border-width: 0px;
-  border-radius: var(--dl-radius-radius-radius8);
-  text-decoration: none;
-  background-color: var(--dl-color-custom-primary2);
-}
 .role-play-list-hero1 {
   padding-top: 0px;
   border-color: rgba(0, 0, 0, 0);
@@ -422,12 +352,16 @@ export default {
 }
 .role-play-list-navlink2 {
   color: var(--dl-color-gray-white);
-  text-align: left;
-  border-color: var(--dl-color-gray-white);
+  font-size: 20px;
+  align-self: center;
+  font-style: normal;
+  font-weight: 600;
   border-width: 0px;
-  border-radius: 0px;
+  margin-right: var(--dl-space-space-twounits);
+  border-radius: var(--dl-radius-radius-radius8);
   text-decoration: none;
-  background-color: transparent;
+  background-color: var(--dl-color-custom-primary2);
+  margin-bottom: var(--dl-space-space-unit);
 }
 .role-play-list-li1 {
   color: var(--dl-color-gray-white);
@@ -439,15 +373,6 @@ export default {
   background-color: var(--dl-color-custom-primary2);
   list-style-image: none;
   list-style-position: outside;
-}
-.role-play-list-navlink3 {
-  color: var(--dl-color-gray-white);
-  text-align: left;
-  border-color: var(--dl-color-gray-white);
-  border-width: 0px;
-  border-radius: 0px;
-  text-decoration: none;
-  background-color: transparent;
 }
 .role-play-list-footer {
   flex: 0 0 auto;
