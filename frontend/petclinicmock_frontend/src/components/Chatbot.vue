c3978f1369b235e6603144cbb312da4020ca5104
Hairful <96092093+Hairful@users.noreply.github.com>
2024-04-10 18:43:43 +0800

    [feat] Chatbot framework

---
+++ frontend/petclinicmock_frontend/src/components/Chatbot.vue
@@ -0,0 +1,37 @@
+<!-- Chatbot.vue -->
+<template>
+    <div class="chatbot" :class="{ expanded: isExpanded }" @click="toggleChatbot">
+        aaaaaa<!-- Your chatbot content goes here -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isExpanded: false,
+        };
+    },
+    methods: {
+        toggleChatbot() {
+            this.isExpanded = !this.isExpanded;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.chatbot {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    /* Add your small logo styles here */
+}
+
+.chatbot.expanded {
+    width: 300px;
+    height: 500px;
+    border: whitesmoke 1px solid;
+    background-color: white;
+}
+</style>
