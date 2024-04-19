79698f27159409fe77f7f18ea8dff91da78b9cc2
SundustYc <SundustYc@outlook.com>
2024-04-19 08:46:15 +0800

    [fix] add missing redis in dependencies

--- backend/src/config/redisClient.js
+++ backend/src/config/redisClient.js
@@ -1,9 +1,31 @@
+/**
+ * 文件: /backend/src/config/redis.js
+ * 描述: Redis连接配置
+ * 作者: {SSR}
+ */
+
 const redis = require('redis');
+
+const loggerConfigurations = [
+    { name: 'admin', level: 'info' },
+    { name: 'error', level: 'error' }
+];
+const logger = require('../utils/logUtil')(loggerConfigurations);
+
 const redisClient = redis.createClient({
-    url: 'redis://localhost:6379'
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
 });
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
-redisClient.connect();
+redisClient.on('error', (err) => {
+    logger.error('Redis Client Error', err); // 使用logger.error替换console.log
+});
+
+redisClient.connect()
+    .then(() => {
+        logger.info('Successfully connected to Redis');
+    })
+    .catch(err => {
+        logger.error('Unable to connect to Redis', err);
+    });
 
 module.exports = redisClient;
